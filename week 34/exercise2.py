fc760be730a7a666a217e657f95237c6557160d5
Andreas Alstad <andralst@ulrik.uio.no>
2023-08-23 11:43:51 +0200

    exercise2

---
+++ week 34/exercise2.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LinearRegression
+
+x = np.random.rand(100,1)
+noise = np.random.randn(100,1)
+y = 2.0 + 5*x**2 + 0.1*noise
+
+############### 1. ############
+my_poly2 = 
+
+############### 2. ############
+
+############### 3. ############
+
