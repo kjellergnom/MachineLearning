c200cf9441beaa3a523b27fc0b5758652a2d20df
Andreas Alstad <andralst@ulrik.uio.no>
2023-08-26 16:03:54 +0200

    Solved exercise 2.1

--- week 34/exercise2.py
+++ week 34/exercise2.py
@@ -4,13 +4,22 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LinearRegression
 
-x = np.random.rand(100,1)
+x = np.linspace(0, 1, 100)
 noise = np.random.randn(100,1)
-y = 2.0 + 5*x**2 + 0.1*noise
-
+y = x**2 + 2.0 + 0.1*np.squeeze(noise)
+print(np.shape(x))
+print(np.shape(y))
 ############### 1. ############
-my_poly2 = 
+design_matrix = np.ones((len(x), 3))
+design_matrix[:, 1] = x[:]
+design_matrix[:, 2] = x[:]**2
+beta = np.linalg.inv(design_matrix.transpose() @ design_matrix) @ design_matrix.transpose() @ y
+y_model = beta[0] + beta[1]*x + beta[2]*x**2
 
+plt.scatter(x, y, c='r', label = 'Data points')
+plt.plot(x, y_model, label = 'LSM')
+plt.legend(loc='best')
+plt.show()
 ############### 2. ############
 
 ############### 3. ############
