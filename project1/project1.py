af0730a5a5900d7c445d0bc2426e5f46afe63d70
andralst <andralst@uio.no>
2023-09-13 16:35:11 +0200

    bleh

--- project1/project1.py
+++ project1/project1.py
@@ -3,12 +3,12 @@ from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
-from random import random, seed
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.linear_model import LinearRegression, Ridge
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 
+### FrankeFunction() and create_X() are taken from the project 1 description
 def FrankeFunction(x,y):
     term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
     term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
@@ -16,27 +16,18 @@ def FrankeFunction(x,y):
     term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
     return term1 + term2 + term3 + term4
 
-def generate_design_matrix(x, n_features, include_bias=True):
-    if include_bias == True:
-        X = np.ones(shape=(n, n_features+1))
-        for feature in range(n_features):
-            X[:, feature+1] = x[:, 0]**(feature+1)
-    else:
-        X = np.zeros(shape=(n, n_features))
-        for feature in range(n_features):
-            X[:, feature] = x[:, 0]**(feature + 1)
-    return X
-
-def design_matrix(x, y, n_features):
-    X = np.zeros(shape=(n, 1))
-    for feature in range(n_features+1):
-        for deg in range(feature//2+1):
-            if (feature == 0 and deg == 0):
-                continue
-            print(f'x^{feature - deg}, y^{deg}')
-            if (feature - deg) != deg:
-                print(f'x^{deg}, y^{feature - deg}')
-        print('\n')
+def create_X(x, y, n):
+	if len(x.shape) > 1:
+		x = np.ravel(x)
+		y = np.ravel(y)
+	N = len(x)
+	l = int((n+1)*(n+2)/2)		# Number of elements in beta
+	X = np.ones((N,l))
+	for i in range(1,n+1):
+		q = int((i)*(i+1)/2)
+		for k in range(i+1):
+			X[:,q+k] = (x**(i-k))*(y**k)
+	return X
 
 def beta_OLS(X, y):
     beta =  np.linalg.pinv(X.T @ X) @ X.T @ y
@@ -47,26 +38,61 @@ def beta_Ridge(X, y, lmbda):
     return beta
 
 # Make data.
+np.random.seed()
 n = 50
-order = 3
-x = np.linspace(0, 1, n)
-y = np.linspace(0, 1, n)
-print(np.shape([x, y]))
+order = 2
+x = np.sort(np.random.uniform(0, 1, n))
+y = np.sort(np.random.uniform(0, 1, n))
+X = create_X(x, y, order)
+noise = np.random.normal(0, 0.05, (n,n))
 x, y = np.meshgrid(x,y)
-noise = np.random.normal(0, 0.02, x.shape)
 z = FrankeFunction(x, y) + noise
-print(np.shape([x, y]))
-# Estimate
-model = make_pipeline(
-    PolynomialFeatures(degree=order), 
-    LinearRegression(fit_intercept=False))
-model.fit((x, y), z)
-z_model = model.predict(np.c_[x.flatten(), y.flatten()]).reshape(x.shape)
+
+# Split data into training and test data
+X_train, X_test, z_train, z_test = train_test_split(X, z, test_size=0.2)
+
+# Scale data
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train_scaled = scaler.transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Estimate and evaluate model using sklearn
+reg = LinearRegression().fit(X_train_scaled, z_train)
+z_tilde_OLS = reg.predict(X_train_scaled)
+z_predict_OLS = reg.predict(X_test_scaled)
+betaOLS = reg.coef_
+
+# Estimate and evaluate model
+# betaOLS = beta_OLS(X_train_scaled, z_train)
+# z_tilde_OLS = X_train_scaled @ betaOLS
+# z_predict_OLS = X_test_scaled @ betaOLS
+MSE_OLS_train = mean_squared_error(z_train, z_tilde_OLS)
+MSE_OLS_test = mean_squared_error(z_test, z_predict_OLS)
+R2_OLS_train = r2_score(z_train, z_tilde_OLS)
+R2_OLS_test = r2_score(z_test, z_predict_OLS)
+
+# Create train and test surfaces
+x_surface_train = X_train_scaled[:, 1]
+x_surface_test = X_test_scaled[:, 1]
+y_surface_train = X_train_scaled[:, 2]
+y_surface_test = X_test_scaled[:, 2]
+xx_train, yy_train = np.meshgrid(x_surface_train, y_surface_train)
+xx_test, yy_test = np.meshgrid(x_surface_test, y_surface_test)
+
+print(np.shape(X))
+print(np.shape(X_train_scaled))
+print(np.shape(X_test_scaled))
+print(np.shape(z_train))
+print(np.shape(z_test))
+print(np.shape(z_tilde_OLS))
+print(np.shape(betaOLS))
 
 # Plot the surface.
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
-surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
+ax.scatter(x, y, z, c='r', marker='o', s=1)
+surf = ax.plot_surface(xx_train, yy_train, z_tilde_OLS, cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
 
 # Customize the z axis.
@@ -76,7 +102,4 @@ ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 
 # Add a color bar which maps values to colors.
 fig.colorbar(surf, shrink=0.5, aspect=5)
-
 plt.show()
-
-#### KLADD ####
