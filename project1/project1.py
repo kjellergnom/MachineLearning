c135e35020cd2f878d89c27dca7a356ad2d1cb7e
Kjellergnom <a.alstad@live.no>
2023-09-14 16:41:38 +0200

    asdf

--- project1/project1.py
+++ project1/project1.py
@@ -7,6 +7,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.pipeline import make_pipeline
 
 ### FrankeFunction() and create_X() are taken from the project 1 description
 def FrankeFunction(x,y):
@@ -26,6 +27,9 @@ def create_X(x, y, n):
 	for i in range(1,n+1):
 		q = int((i)*(i+1)/2)
 		for k in range(i+1):
+			print(f'q+k = {q+k}')
+			print(f'i-k = {i-k}')
+			print(f'k = {k}')
 			X[:,q+k] = (x**(i-k))*(y**k)
 	return X
 
@@ -39,8 +43,9 @@ def beta_Ridge(X, y, lmbda):
 
 # Make data.
 np.random.seed()
-n = 50
-order = 2
+n = 70
+order = 5
+test_size = 0.2
 x = np.sort(np.random.uniform(0, 1, n))
 y = np.sort(np.random.uniform(0, 1, n))
 X = create_X(x, y, order)
@@ -49,51 +54,44 @@ x, y = np.meshgrid(x,y)
 z = FrankeFunction(x, y) + noise
 
 # Split data into training and test data
-X_train, X_test, z_train, z_test = train_test_split(X, z, test_size=0.2)
+X_train, X_test, z_train, z_test = train_test_split(X, z, test_size=test_size, shuffle=False)
 
-# Scale data
+# Scale data using sklearn
 scaler = StandardScaler()
 scaler.fit(X_train)
 X_train_scaled = scaler.transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-# Estimate and evaluate model using sklearn
-reg = LinearRegression().fit(X_train_scaled, z_train)
-z_tilde_OLS = reg.predict(X_train_scaled)
-z_predict_OLS = reg.predict(X_test_scaled)
-betaOLS = reg.coef_
-
 # Estimate and evaluate model
-# betaOLS = beta_OLS(X_train_scaled, z_train)
-# z_tilde_OLS = X_train_scaled @ betaOLS
-# z_predict_OLS = X_test_scaled @ betaOLS
-MSE_OLS_train = mean_squared_error(z_train, z_tilde_OLS)
-MSE_OLS_test = mean_squared_error(z_test, z_predict_OLS)
-R2_OLS_train = r2_score(z_train, z_tilde_OLS)
-R2_OLS_test = r2_score(z_test, z_predict_OLS)
+model = make_pipeline(PolynomialFeatures(order), LinearRegression())
+model.fit(X_train_scaled, z_train)
+z_fit_OLS = model.predict(X_train_scaled)
+z_pred_OLS = model.predict(X_test_scaled)
+z_fit_OLS = X_train_scaled @ beta_OLS(X_train_scaled, z_train)
+z_pred_OLS = X_test_scaled @ beta_OLS(X_test_scaled, z_test)
+print(z_fit_OLS)
+print(z_pred_OLS)
+mse_OLS = mean_squared_error(z_test, z_pred_OLS)
+r2_OLS = r2_score(z_test, z_pred_OLS)
+print(f'Test MSE OLS = {mse_OLS:.4f}')
+print(f'Test R2 OLS = {r2_OLS:.4f}')
 
 # Create train and test surfaces
-x_surface_train = X_train_scaled[:, 1]
-x_surface_test = X_test_scaled[:, 1]
-y_surface_train = X_train_scaled[:, 2]
-y_surface_test = X_test_scaled[:, 2]
-xx_train, yy_train = np.meshgrid(x_surface_train, y_surface_train)
-xx_test, yy_test = np.meshgrid(x_surface_test, y_surface_test)
-
-print(np.shape(X))
-print(np.shape(X_train_scaled))
-print(np.shape(X_test_scaled))
-print(np.shape(z_train))
-print(np.shape(z_test))
-print(np.shape(z_tilde_OLS))
-print(np.shape(betaOLS))
+mesh_len_train = np.shape(X_train_scaled)[0]
+mesh_len_test = np.shape(X_test_scaled)[0]
+xx_train, yy_train = np.meshgrid(np.linspace(0, 1, mesh_len_train), np.linspace(0, 1, mesh_len_train))
+xx_test, yy_test = np.meshgrid(np.linspace(0, 1, mesh_len_test), np.linspace(0, 1, mesh_len_test))
 
 # Plot the surface.
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
-ax.scatter(x, y, z, c='r', marker='o', s=1)
-surf = ax.plot_surface(xx_train, yy_train, z_tilde_OLS, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
+ax.scatter(x, y, z, c='k', marker='o', s=2, label='Franke function')
+# ax.scatter(xx_train, yy_train, z_fit_OLS[:, :mesh_len_train], c='r', marker='o', s=2, label='OLS fit')
+# ax.scatter(xx_test, yy_test, z_pred_OLS[:, :mesh_len_test], c='g', marker='o', s=2, label='OLS prediction')
+# surf_train = ax.plot_surface(xx_train, yy_train, z_fit_OLS[:, :mesh_len_train], cmap=cm.coolwarm,
+#                        linewidth=0, antialiased=False, label='OLS fit', alpha=0.5)
+surf_test = ax.plot_surface(xx_test, yy_test, z_pred_OLS[:, :mesh_len_test], cmap=cm.plasma,
+					   linewidth=0, antialiased=False, label='OLS prediction', alpha=0.5)
 
 # Customize the z axis.
 ax.set_zlim(-0.10, 1.40)
@@ -101,5 +99,10 @@ ax.zaxis.set_major_locator(LinearLocator(10))
 ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 
 # Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
+# fig.colorbar(surf_train, shrink=0.5, aspect=5)
+fig.colorbar(surf_test, shrink=0.5, aspect=5)
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+# plt.legend(loc='best')
 plt.show()
