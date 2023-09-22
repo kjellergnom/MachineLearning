38122a17ca237de26c3e6286f7608ee46f0da69a
andralst <andralst@uio.no>
2023-09-22 15:56:46 +0200

    cleaner, slower, and (now) functional

--- week38/week38.py
+++ week38/week38.py
@@ -21,53 +21,29 @@ def least_squares_method(X, y):
 def get_bias(x, y):
     return (x - np.mean(y))**2
 
-np.random.seed()
+np.random.seed(2018)
 n = 100
+max_deg = 15
+n_bootstraps = 100
+
 x = np.linspace(-3, 3, n).reshape(-1, 1)
 y = np.exp(-x**2) + 1.5 * np.exp(-(x-2)**2) + np.random.normal(0, 0.1, x.shape)
-n_bootstraps = 100
-deg = 15
-poly_degs = [x for x in range(16)]
-
-mse_list = np.zeros(max(poly_degs)+1)
-var_list = np.zeros(max(poly_degs)+1)
-bias_list = np.zeros(max(poly_degs)+1)
+poly_degs = [x for x in range(max_deg+1)]
+mse_list = np.zeros(len(poly_degs))
+var_list = np.zeros(len(poly_degs))
+bias_list = np.zeros(len(poly_degs))
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 for deg in poly_degs:
-    X = generate_design_matrix(x, deg)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-    # Scale data
-    scaler = StandardScaler(with_std=False)
-    scaler.fit(X_train)
-    X_train_scaled = scaler.transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-
-    y_scaler = StandardScaler(with_std=False)
-    y_scaler.fit(y_train)
-    y_train_scaled = y_scaler.transform(y_train)
-    y_test_scaled = y_scaler.transform(y_test)
-
     # Fit model
-    # model = pipe.make_pipeline(PolynomialFeatures(deg=deg), LinearRegression())
-    # model.fit(X_train_scaled, y_train_scaled)
-    # y_pred = model.predict(X_test_scaled)
-    y_pred = np.empty((y_test_scaled.shape[0], n_bootstraps))
+    model = pipe.make_pipeline(StandardScaler(), PolynomialFeatures(degree=deg), LinearRegression(fit_intercept=False))
+    y_pred = np.empty((y_test.shape[0], n_bootstraps))
     for i in range(n_bootstraps):
-        X_, y_ = resample(X_train_scaled, y_train_scaled)
-        y_pred[:, i] = least_squares_method(X_test_scaled, y_test_scaled).ravel()
-    print(y_pred.shape)
-    # Reintroduce the intercept
-    y_intercept = y_scaler.mean_
-
-    # Sort arrays so that they become line-plottable
-    X_test_plot, y_pred_plot = zip(*sorted(zip(X_test_scaled[:, 1], least_squares_method(X_test_scaled, y_test_scaled))))
-
-    # MSE, variance and bias
-    # MSE = mean_squared_error(y_test_scaled, y_pred)
-    MSE = np.mean(np.mean((y_test_scaled - y_pred)**2, axis=1, keepdims=True))
-    # bias = np.mean(get_bias(y_test_scaled, y_pred))
-    bias = np.mean((y_test_scaled - np.mean(y_pred, axis=1, keepdims=True))**2)
+        x_, y_ = resample(x_train, y_train)
+        y_pred[:, i] = model.fit(x_, y_).predict(x_test).ravel()
+    
+    MSE = np.mean(np.mean((y_test - y_pred)**2, axis=1, keepdims=True))
+    bias = np.mean((y_test - np.mean(y_pred, axis=1, keepdims=True))**2)
     var = np.mean(np.var(y_pred, axis=1, keepdims=True))
     mse_list[deg] = MSE
     var_list[deg] = var
@@ -76,18 +52,18 @@ for deg in poly_degs:
     print('Error:', mse_list[deg])
     print('Bias^2:', bias_list[deg])
     print('Var:', var_list[deg])
-    print('{} >= {} + {} = {}'.format(mse_list[deg], bias_list[deg], var_list[deg], bias_list[deg]+var_list[deg]))
+    print(f'Error = Bias^2 + Var: {mse_list[deg]} = {bias_list[deg]} + {var_list[deg]}')
 
 
 # Plot main plot
-fig, ax = plt.subplots()
-ax.scatter(x, y, label='Data', color='r', marker='x')
-# ax.scatter(X_train_scaled[:, 1], y_train_scaled, label='Train data', color='g', marker='o')
-ax.plot(X_test_plot, y_pred_plot+y_intercept, 'b--', label='Prediction')
-ax.set_xlabel(r'$x$')
-ax.set_ylabel(r'$y$')
-ax.legend(loc='best')
-plt.show()
+# fig, ax = plt.subplots()
+# ax.scatter(x, y, label='Data', color='r', marker='x')
+# # ax.scatter(X_train_scaled[:, 1], y_train_scaled, label='Train data', color='g', marker='o')
+# ax.plot(X_test_plot, y_pred_plot+y_intercept, 'b--', label='Prediction')
+# ax.set_xlabel(r'$x$')
+# ax.set_ylabel(r'$y$')
+# ax.legend(loc='best')
+# plt.show()
 
 # Plot metrics
 fig, ax = plt.subplots()
